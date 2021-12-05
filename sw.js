4a4499763a5ede673ae23e00ce15161e3ca4ce08
Zhenbang233 <wzb1244@qq.com>
2021-12-05 16:49:29 +0800

    first

---
+++ sw.js
@@ -0,0 +1,43 @@
+var cacheStorageKey = 'ep-1.0'
+var cacheList=[
+  "/",
+  'index.html',
+  'bg.jpg',
+  "logo.png",
+  "manifest.json",
+];
+function addcache()
+{
+	caches.open(cacheStorageKey).then(function(cache) {
+		return cache.addAll(cacheList);
+    });
+	self.skipWaiting();
+}
+self.addEventListener('install',e =>{
+  e.waitUntil(addcache());
+})
+self.addEventListener('fetch',function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(response){
+      if(response != null){
+        return response
+      }
+      return fetch(e.request.url)
+    })
+  )
+})
+self.addEventListener('activate',function(e){
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheNames => {
+          return (cacheNames !== cacheStorageKey)
+        }).map(cacheNames => {
+          return caches.delete(cacheNames)
+        })
+      )
+    }).then(() => {
+      return self.clients.claim()
+    })
+  )
+})
